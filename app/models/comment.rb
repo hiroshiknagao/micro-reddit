ca5764a2dfd7b98b3cb95746bfe9b57a87d1f097
hiroshi nagao <hi_1205@hotmail.com>
2024-03-25 17:28:23 +0900

    add comment and passing the test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,7 @@
 class Comment < ApplicationRecord
- 
+  belongs_to :user
+  belongs_to :post
 
   validates :content, presence: true
   validates :content, presence: true, length: { minimum: 5, maximum: 200 }
-
-  belongs_to :user
-  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 20, maximum: 200 }
 
